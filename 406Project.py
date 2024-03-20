7d8d580244c2e3956873eae7160389f51e58b45c
Bowie Chau <132609756+b-chau23@users.noreply.github.com>
2024-03-20 01:38:26 +0000

    created members class

--- 406Project.py
+++ 406Project.py
@@ -1,1 +1,7 @@
-print("hello world")
+class Members():
+    def __init__(self, name, phone_num, address, paid, attended_class) -> None:
+        self.name = name
+        self.phone_number = phone_num
+        self.address = address
+        self.paid = paid
+        self.attended_class = attended_class
